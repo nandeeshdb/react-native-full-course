ebbcf9a834910849e5e250473ad07dc1ccc9b6b7
nandeesh_007 <nandeesh.db@bulkbeings.co>
2024-09-13 18:40:13 +0530

    Adding stylyes to react-native components

--- App.tsx
+++ App.tsx
@@ -1,15 +1,35 @@
-import React from 'react'
-import { SafeAreaView, Text, View } from 'react-native'
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native';
 
 function App() {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello word</Text>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.blackText}>
+          Hello world
+        </Text>
       </View>
-      
     </SafeAreaView>
-  )
+  );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1, // Ensures SafeAreaView takes up the full screen
+  },
+  container: {
+    flex: 1, // Makes the container take up the full available space
+    alignItems: 'center', // Centers text horizontally
+    justifyContent: 'center', // Centers text vertically
+  },
+  blackText: {
+    color: '#000000',
+  },
+});
