961f15bd66dcbdd98664d6c0d6f1dc99ca16e7b9
nandeesh_007 <nandeesh.db@bulkbeings.co>
2024-09-16 10:46:49 +0530

    Flex Cards

--- components/flat-card.tsx
+++ components/flat-card.tsx
@@ -1,12 +1,44 @@
-import { Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 export default function FlatCard() {
   return (
-    <View>
-      <Text>flat-card</Text>
+    <View style={styles.container}>
+      <View style={[styles.cardContainer, styles.cardOne]}>
+        <Text>Red</Text>
+      </View>
+      <View style={[styles.cardContainer, styles.cardTwo]}>
+        <Text>Blue</Text>
+      </View>
+      <View style={[styles.cardContainer, styles.cardThree]}>
+        <Text>Green</Text>
+      </View>
     </View>
   );
 }
 
-//const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 8,
+    gap: 10,
+  },
+  cardContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+    width: 100,
+    height: 100,
+  },
+  cardOne: {
+    backgroundColor: '#FF0000',
+  },
+  cardTwo: {
+    backgroundColor: '#0000FF',
+  },
+  cardThree: {
+    backgroundColor: '#00FF00',
+  },
+});
